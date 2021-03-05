9ec941a473b7a162f23d1fbcec45678117281437
Lennox Luis <71387094+LennoxLuis@users.noreply.github.com>
2021-03-05 17:26:56 +0800

    Update and rename README.md to variables.dart

---
+++ variables.dart
@@ -0,0 +1,14 @@
+void main() {
+  var firstname = 'jollibee';
+  var middleInitial = 'E';
+  var lastName = 'lopez';
+    var age = 42;
+  var height = 1.83;
+  
+  
+  print(firstname);
+  print (middleInitial);
+  print (lastName);
+  print(age);
+  print(height);
+}
